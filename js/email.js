6a4613725fea816253a2230279cd9ce87ec1d0c6
Juan Corso <juanadriancorso94@gmail.com>
2022-03-21 08:41:56 -0300

    reparacion de sliders

---
+++ js/email.js
@@ -0,0 +1,17 @@
+const formulario = document.querySelector('#form');
+console.log(formulario);
+
+
+formulario.addEventListener('submit', subirDatos);
+
+function subirDatos(event) {
+
+    event.preventDefault();
+
+    const form = new FormData(this)
+    console.log(form.get('nombre'))
+    console.log(form.get('email'))
+    console.log(form.get('asunto'))
+    console.log(form.get('contenido-texto'))
+
+}
